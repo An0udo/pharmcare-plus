2c2ad6a759dc3c07add9e5792dbe18014c19a78b
Alanoud JHD <alanoudjhd@alMacBook-Air-alkhas-bAlanoud.local>
2024-07-22 08:30:02 +0300

    Add home route

--- process_drugs.py
+++ process_drugs.py
@@ -20,6 +20,10 @@ logging.basicConfig(filename="error.log", level=logging.ERROR)
 app = Flask(__name__)
 CORS(app, resources={r"/process_drugs": {"origins": "*", "methods": ["POST", "OPTIONS"]}})
 
+@app.route('/')
+def home():
+    return "Welcome to Pharmacare+"
+
 # Load models and vectorizer (using dynamic import for better organization)
 def load_model_and_vectorizer():
     try:
